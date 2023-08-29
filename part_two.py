b6e0b505f8dcca389bf3241a6bf57dddd0865f3f
Matt <mat.hop@gmail.com>
2023-08-29 09:36:26 +0100

    part_two fixed

--- part_two.py
+++ part_two.py
@@ -1,9 +1,13 @@
 try:
     import numpy as np
-    print('Well done installed correctly')
-    print("Let's look at part_three.py")
-    exit()
-except:
+    np_response = np.round(5.2)
+
+finally:
+    if np_response == 5:
+        print('Well done installed correctly')
+        print("Let's look at part_three.py")
+        exit()
+else:
     print("Sorry you haven't installed the requirements file")
     print("Please ensure you have created a virtual environment")
     print("and installed 'secret_req.txt' requirements file.")
