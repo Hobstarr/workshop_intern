38b1ce7e874b141bdded6d065233d3e2c03f7797
Matt <mat.hop@gmail.com>
2023-08-29 09:37:19 +0100

    editted

--- part_two.py
+++ part_two.py
@@ -6,9 +6,9 @@ finally:
     if np_response == 5:
         print('Well done installed correctly')
         print("Let's look at part_three.py")
+        
+    else:
+        print("Sorry you haven't installed the requirements file")
+        print("Please ensure you have created a virtual environment")
+        print("and installed 'secret_req.txt' requirements file.")
         exit()
-else:
-    print("Sorry you haven't installed the requirements file")
-    print("Please ensure you have created a virtual environment")
-    print("and installed 'secret_req.txt' requirements file.")
-    exit()
